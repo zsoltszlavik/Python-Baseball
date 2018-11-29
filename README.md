12997a53d7663d60598843847313b2d915e124a3
Tom Bell <tjbell@gmail.com>
2018-11-29 15:49:26 -0700

    refactor: new repo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Building Baseball Data Visualizations with Python - Draft Project
-Not ready for use
+# Python-Baseball
---
+++ attendance.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+games_directory = os.path.join(os.getcwd(), 'games')
+files = [fn for fn in os.listdir(games_directory) if fn.endswith(".EVE")]
+files.sort()
+
+attendances = []
+for file in files:
+  with open (os.path.join(os.getcwd(), 'games', file), 'rt') as in_file:
+    for line in in_file:
+      row = line.rstrip().split(',')
+      if 'attendance' in row:
+        attendances.append([file, row[2]])
+
+data = pd.DataFrame(attendances)
+data.columns = ["year", "attendance"]
+
+data.year = pd.to_numeric(data.year.str.extract(r'^(\d{4})', expand=False))
+data.attendance = pd.to_numeric(data.attendance)
+
+data.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+plt.axhline(y=data.attendance.mean(), label='Mean', linestyle='--', color='green')
+
+plt.show()
