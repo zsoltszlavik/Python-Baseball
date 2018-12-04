39124a438932d3213e93ec3d5a72db48a128058a
Tom Bell <tjbell@gmail.com>
2018-12-04 15:44:38 -0700

    test: module1 test

--- stats/attendance.py
+++ stats/attendance.py
@@ -2,28 +2,28 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-games_directory = os.path.join(os.getcwd(), 'games')
-files = [fn for fn in os.listdir(games_directory) if fn.endswith(".EVE")]
-files.sort()
+# games_directory = os.path.join(os.getcwd(), 'games')
+# files = [fn for fn in os.listdir(games_directory) if fn.endswith(".EVE")]
+# files.sort()
 
-attendances = []
-for file in files:
-  with open (os.path.join(os.getcwd(), 'games', file), 'rt') as current:
-    for line in current:
-      row = line.rstrip().split(',')
-      if 'attendance' in row:
-        attendances.append([file, row[2]])
+# attendances = []
+# for file in files:
+#   with open (os.path.join(os.getcwd(), 'games', file), 'rt') as current:
+#     for line in current:
+#       row = line.rstrip().split(',')
+#       if 'attendance' in row:
+#         attendances.append([file, row[2]])
 
-data = pd.DataFrame(attendances)
-data.columns = ["year", "attendance"]
+# data = pd.DataFrame(attendances)
+# data.columns = ["year", "attendance"]
 
-year = data.year.str.extract(r'^(\d{4})', expand=False)
-data.year = pd.to_numeric(year)
-data.attendance = pd.to_numeric(data.attendance)
+# year = data.year.str.extract(r'^(\d{4})', expand=False)
+# data.year = pd.to_numeric(year)
+# data.attendance = pd.to_numeric(data.attendance)
 
-data.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
-plt.xlabel('Year')
-plt.ylabel('Attendance')
-plt.axhline(y=data.attendance.mean(), label='Mean', linestyle='--', color='green')
+# data.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+# plt.xlabel('Year')
+# plt.ylabel('Attendance')
+# plt.axhline(y=data.attendance.mean(), label='Mean', linestyle='--', color='green')
 
-plt.show()
+# plt.show()
--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,40 +1,48 @@
 import pytest
+import matplotlib
+matplotlib.use('Agg')
 
 from .utils import *
-
 from stats import attendance
-
 
 @pytest.mark.test_imports_module1
 def test_imports_module1():
-    assert 'Flask' in dir(app), 'Have you imported the `Flask` class from `flask`?'
-    assert inspect.isclass(app.Flask), '`Flask` is not a class.'
-    assert 'render_template' in dir(app), '`render_template` has not been imported.'
-    assert inspect.isfunction(app.render_template), '`render_template` is not a function.'
+    assert 'os' in dir(attendance), 'Have you imported the `os` built-in library?'
+    assert 'pd' in dir(attendance), 'Have you imported `pandas` as `pd`?'
+    assert 'plt' in dir(attendance), 'Have you imported the `matplotlib.pyplot` as `plt`?'
 
 @pytest.mark.test_event_files_module1
 def test_event_files_module1():
+    pass
 
 @pytest.mark.test_loop_through_files_module1
 def test_loop_through_files_module1():
+    pass
 
 @pytest.mark.test_open_event_files_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_loop_through_lines_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_find_attendances_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_pandas_dataframe_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_convert_to_numeric_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_plot_dataframe_module1
 def test_app_import_flask_module1():
+    pass
 
 @pytest.mark.test_formatting_and_mean_module1
 def test_app_import_flask_module1():
+    pass
