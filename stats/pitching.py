451821673f41f86a82182609c57b88115c25115c
Tom Bell <tjbell@gmail.com>
2019-02-12 16:50:16 -0700

    module3-solution

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games
+
+plays = games[games['type'] == 'play']
+
+strike_outs = plays[plays['event'].str.contains('K')]
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+strike_outs = strike_outs.reset_index(name='strike_outs')
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+strike_outs.plot( y='strike_outs', x='year', kind='scatter').legend(['Strike Outs'])
+
+plt.show()
