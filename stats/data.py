1f36f8c3783b569dad9ba64d0a27d6be3cd59c4c
Zsolt Szlavik <zsoltszlavik@gmail.com>
2020-06-21 20:50:15 +0200

    Completed module - added comments

--- stats/data.py
+++ stats/data.py
@@ -15,12 +15,16 @@ games = pd.concat(game_frames)
 
 games.loc[games['multi5'] == '??', ['multi5']] = ''
 
+#Call the extract() function of the str namespace on this column. Hint: dataframe['column'].str.extract()
+#extract() function returns a DataFrame
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
 
 games = pd.concat([games, identifiers], sort=False, axis=1)
 games = games.fillna(' ')
+
+#o slightly reduce the memory used by the games DataFrame we can provide Pandas with a clue to what data is contained in certain columns.
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 
 print(games.head())
