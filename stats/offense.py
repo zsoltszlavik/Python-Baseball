fa78677b21c5f63568116a8cafe52191a29250a3
Zsolt Szlavik <zsoltszlavik@gmail.com>
2020-07-26 08:55:08 +0200

    Completed module 4 - mod

--- stats/offense.py
+++ stats/offense.py
@@ -7,14 +7,11 @@ plays =  games[games['type'] == 'play']
 
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)',regex=True), ['inning','event']]
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning','event']]
 
 hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
-replacements = {r'^S(.*)': 'single',
-r'^D(.*)': 'double',
-r'^T(.*)': 'triple',
-r'^HR(.*)': 'hr'}
+replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
 
 hit_type = hits['event'].replace(replacements, regex=True)
 
@@ -22,17 +19,15 @@ hits = hits.assign(hit_type=hit_type)
 
 #print(hits.head)
 
-counts = hits.groupby(['inning','hit_type']).size()
-
-hits = counts.reset_index(name='counts')
+hits = hits.groupby(['inning','hit_type']).size().reset_index(name='count')
 
 #print(hits.head)
 
-hits.loc[:,'hit_type'] = pd.Categorical(hits.loc[:,'hit_type'], ['single', 'double', 'triple', 'hr'])
+hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
 
 hits = hits.sort_values(['inning', 'hit_type'])
 
-hits = hits.pivot(index='inning', columns='hit_type', values='counts')
+hits = hits.pivot(index='inning', columns='hit_type', values='count')
 
 hits.plot.bar(stacked=True)
 
